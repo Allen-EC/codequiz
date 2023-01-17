b13bc1c803a3209606dc2f93ede9a86fb9d911e4
Allen-EC <allen.e.cummins@googlemail.com>
2023-01-17 10:34:38 +0000

    Added dummy questions, and functions to randomise the question set, and to pick a question

---
+++ assets/js/questions.js
@@ -0,0 +1,138 @@
+let quizQuestions = [];
+let quizQuestion = {};
+let rquizQuestions = [];
+
+// Dummy questions to test the blueprint for the question structure.
+// After the application has been tested and deamed to be functioning correctly,
+// these questions can be replaced with real questions.
+
+
+quizQuestion = 
+{
+	"question" : "Question 1",
+	"correctAnswer" : "1Correct answer",
+	"1stIncorrect" : "1Incorrect 1",
+	"2ndIncorrect" : "1Incorrect 2",
+	"3rdIncorrect" : "1Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 2",
+	"correctAnswer" : "2Correct answer",
+	"1stIncorrect" : "2Incorrect 1",
+	"2ndIncorrect" : "2Incorrect 2",
+	"3rdIncorrect" : "2Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 3",
+	"correctAnswer" : "3Correct answer",
+	"1stIncorrect" : "3Incorrect 1",
+	"2ndIncorrect" : "3Incorrect 2",
+	"3rdIncorrect" : "3Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 4",
+	"correctAnswer" : "4Correct answer",
+	"1stIncorrect" : "4Incorrect 1",
+	"2ndIncorrect" : "4Incorrect 2",
+	"3rdIncorrect" : "4Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 5",
+	"correctAnswer" : "5Correct answer",
+	"1stIncorrect" : "5Incorrect 1",
+	"2ndIncorrect" : "5Incorrect 2",
+	"3rdIncorrect" : "5Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 6",
+	"correctAnswer" : "6Correct answer",
+	"1stIncorrect" : "6Incorrect 1",
+	"2ndIncorrect" : "6Incorrect 2",
+	"3rdIncorrect" : "6Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 7",
+	"correctAnswer" : "7Correct answer",
+	"1stIncorrect" : "7Incorrect 1",
+	"2ndIncorrect" : "7Incorrect 2",
+	"3rdIncorrect" : "7Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 8",
+	"correctAnswer" : "8Correct answer",
+	"1stIncorrect" : "8Incorrect 1",
+	"2ndIncorrect" : "8Incorrect 2",
+	"3rdIncorrect" : "8Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 9",
+	"correctAnswer" : "9Correct answer",
+	"1stIncorrect" : "9Incorrect 1",
+	"2ndIncorrect" : "9Incorrect 2",
+	"3rdIncorrect" : "9Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+quizQuestion = 
+{
+	"question" : "Question 10",
+	"correctAnswer" : "10Correct answer",
+	"1stIncorrect" : "10Incorrect 1",
+	"2ndIncorrect" : "10Incorrect 2",
+	"3rdIncorrect" : "10Incorrect 3"
+}
+quizQuestions.push(quizQuestion);
+
+// Function to pick a random element from an array of questions.
+// The function will then remove the question from the array to avoid duplication of questions.
+
+function pickQuestion(pickQuestions)
+{
+	if(pickQuestions.length === 0)
+	{
+		return false;
+	}
+	var arrayIndex = Math.floor(Math.random() * pickQuestions.length);
+	var returnQuestion = pickQuestions[arrayIndex];
+	pickQuestions.splice(arrayIndex,1);
+	return returnQuestion;
+}
+
+// Create a new array with the questions randomised so the quiz is different every time.
+
+//console.log(tmpQuestions);
+
+function randomiseQuestions()
+{
+	let tmpQuestions = quizQuestions.slice();
+	rquizQuestions = [];
+	while(tmpQuestions.length > 0)
+	{
+		rquizQuestions.push(pickQuestion(tmpQuestions));
+	}
+	console.log(rquizQuestions);
+}
